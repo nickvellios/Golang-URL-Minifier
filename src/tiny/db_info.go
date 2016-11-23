404cb1c79e070962226bc2c2112e60bcb7b6bfdd
Nick Vellios <nick@vellios.com>
2016-11-23 13:38:43 -0800

    Initial Commit

---
+++ src/tiny/db_info.go
@@ -0,0 +1,48 @@
+// URL Minifier
+// Database Configuration
+// Nick Vellios
+// 11/14/2016
+
+/*
+POSTGRESQL Schema
+
+CREATE TABLE url_map (
+	id SERIAL PRIMARY KEY,
+	path VARCHAR(16),
+	url TEXT,
+	t_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	ip VARCHAR(39)
+);
+*/
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+var db *sql.DB
+
+// Database configuration
+
+const (
+	DB_USER     = "username"
+	DB_PASSWORD = "password"
+	DB_NAME     = "database"
+)
+
+// Global DB error checking
+func checkDBErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Open a connection to our database and return a pointer to the instance of the DB connection
+func openDB() *sql.DB {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", dbinfo)
+	checkDBErr(err)
+	return db
+}
